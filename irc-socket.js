9fb054b99f218d0f3c2536c0ebe98aec845a1bf3
rickihastings <rickihastings22@gmail.com>
2013-12-03 13:53:43 +0000

    Moved setupEvents from the prototype and into a function which is called on connect, that sets up all the other event listeners

--- irc-socket.js
+++ irc-socket.js
@@ -12,10 +12,10 @@ var tls = require('tls');
 var events = require('events');
 var util = require('util');
 
-//  var log = function (input, msg) {
-//      var date = new Date();
-//      console.log(Date().toString() + "|" + (input ? "<-" : "->") + "|" + msg);
-//  };
+ var log = function (input, msg) {
+     var date = new Date();
+     console.log(Date().toString() + "|" + (input ? "<-" : "->") + "|" + msg);
+ };
 
 var create = function (prototype, properties) {
     if (typeof properties == 'object') {
@@ -34,9 +34,69 @@ var Socket = module.exports = function Socket (network, GenericSocket) {
     var socket = create(Socket.prototype);
     socket.port = network.port || 6667;
     socket.netname = network.server;
+    socket.secure = network.secure || false;
     socket.network = network;
     socket.genericSocket = new GenericSocket();
     socket.connected = false;
+
+    socket.setupEvents = function () {
+        void function connectEvent () {
+            socket.connected = true;
+            socket.raw(["NICK", socket.network.nick]);
+            socket.raw(["USER", socket.network.user || "user", "8 * :" + socket.network.realname]);
+        }();
+        
+        var onData = function onData (data) {
+            lines = data.split('\r\n');
+
+            if (onData.buffer) {
+                lines[0] = onData.buffer + lines[0];
+                onData.buffer = null;
+            }
+
+            if (lines[lines.length - 1] !== "") {
+                onData.buffer = lines.pop();
+            }
+
+            lines
+            .filter(function (line) { return line !== ''; })
+            .filter(function (line) {
+                if (line.slice(0, 4) === 'PING') {
+                    socket.raw(['PONG', line.slice(line.indexOf(':'))]);
+                    return false;
+                }
+
+                return true;
+            })
+            .forEach(function (line) {
+                log(true, line);
+                socket.emit('data', line);
+            });
+        };
+
+        onData.buffer = null;
+
+        void function readyEvent () {
+            var emitWhenReady = function (data) {
+                if (Socket.isReady(data)) {
+                    socket.emit('ready');
+                }
+            };
+
+            socket.genericSocket.on('data', emitWhenReady);
+            socket.on('ready', function remove () {
+                socket.genericSocket.removeListener('data', emitWhenReady);
+            });
+        }();
+
+        socket.genericSocket.once('close', function () {
+            socket.connected = false;
+        });
+
+        socket.genericSocket.on('data', onData);
+        socket.genericSocket.setEncoding('ascii');
+        socket.genericSocket.setNoDelay();
+    };
 
     return socket;
 };
@@ -54,14 +114,12 @@ Socket.prototype = create(events.EventEmitter.prototype, {
             return;
         }
 
-        if (this.network.secure) {
-            this.genericSocket = tls.connect(this.port, this.netname, {rejectUnauthorized: false});
+        if (this.secure) {
+            this.genericSocket = tls.connect(this.port, this.netname, {rejectUnauthorized: false}, this.setupEvents);
             // set rejectUnauthorized because most IRC servers don't have certified certificates anyway.
         } else {
-            this.genericSocket = this.genericSocket.connect(this.port, this.netname);
+            this.genericSocket = this.genericSocket.connect(this.port, this.netname, this.setupEvents);
         }
-
-        this.setupEvents();
     },
 
     end : function () {
@@ -70,72 +128,6 @@ Socket.prototype = create(events.EventEmitter.prototype, {
         }
 
         this.genericSocket.end();
-    },
-
-    setupEvents : function () {
-        var self = this;
-
-        var onData = function onData (data) {
-            lines = data.split('\r\n');
-
-            if (onData.buffer) {
-                lines[0] = onData.buffer + lines[0];
-                onData.buffer = null;
-            }
-
-            if (lines[lines.length - 1] !== "") {
-                onData.buffer = lines.pop();
-            }
-
-            lines
-            .filter(function (line) { return line !== ''; })
-            .filter(function (line) {
-                if (line.slice(0, 4) === 'PING') {
-                    self.raw(['PONG', line.slice(line.indexOf(':'))]);
-                    return false;
-                }
-
-                return true;
-            })
-            .forEach(function (line) {
-                //log(true, line);
-                self.emit('data', line);
-            });
-        };
-
-        onData.buffer = null;
-
-        void function readyEvent () {
-            var emitWhenReady = function (data) {
-                if (Socket.isReady(data)) {
-                    self.emit('ready');
-                }
-            };
-
-            self.genericSocket.on('data', emitWhenReady);
-            self.on('ready', function remove () {
-                self.genericSocket.removeListener('data', emitWhenReady);
-            });
-        }();
-
-        void function connectEvent () {
-            var emitEvent = (self.network.secure) ? 'secureConnect' : 'connect';
-            var emitWhenConnected = function() {
-                self.connected = true;
-                self.raw(["NICK", self.network.nick]);
-                self.raw(["USER", self.network.user || "user", "8 * :" + self.network.realname]);
-            };
-
-            self.genericSocket.once(emitEvent, emitWhenConnected);
-        }();
-
-        this.genericSocket.once('close', function () {
-            self.connected = false;
-        });
-
-        this.genericSocket.on('data', onData);
-        this.genericSocket.setEncoding('ascii');
-        this.genericSocket.setNoDelay();
     },
 
     raw : function (message) {
@@ -151,7 +143,7 @@ Socket.prototype = create(events.EventEmitter.prototype, {
             throw new Error('Newline detected in message. Use multiple raws instead.');
         }
 
-        //log(false, message);
+        log(false, message);
         this.genericSocket.write(message + '\n', 'ascii');
     },
 
